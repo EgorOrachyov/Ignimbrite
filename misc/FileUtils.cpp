87a67d96997c5067e9875c68bcaff93f75a06633
egororachyov <egororachyov5@gmail.com>
2019-10-26 19:13:41 +0300

    Add ID and ID buffer. Add some tests

---
+++ misc/FileUtils.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Egor Orachyov on 2019-10-26.
+//
+
+#include <FileUtils.h>
+#include <fstream>
+
+void FileUtils::loadData(const std::string &filename, std::vector<char> &data) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("Cannot open file");
+    }
+
+    size_t size = (size_t) file.tellg();
+    data.resize(size);
+    file.seekg(0);
+    file.read(data.data(), size);
+}
---
+++ tests/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Egor Orachyov on 2019-10-26.
+//
+
+#include <TestObjectID.h>
+
+int main() {
+    TestObjectIDBuffer::run();
+    return 0;
+}
