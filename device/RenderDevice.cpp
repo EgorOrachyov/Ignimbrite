f17953beafc186bfe2fc7a5f799b3401179b572f
egororachyov <egororachyov5@gmail.com>
2019-10-27 14:19:24 +0300

    Setup device cmake lib

---
+++ device/RenderDevice.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Egor Orachyov on 2019-10-27.
+//
+
+#include <RenderDevice.h>
+
+const std::string& RenderDevice::getDeviceName() const {
+    return "Default Device";
+}
+
+const std::string& RenderDevice::getVendor() const {
+    return "Default Vendor";
+}
--- device/RenderDevice.h
+++ device/RenderDevice.h
@@ -5,21 +5,28 @@
 #ifndef VULKANRENDERER_RENDERDEVICE_H
 #define VULKANRENDERER_RENDERDEVICE_H
 
+#include <ObjectID.h>
 #include <string>
 
 /**
  * Rendering device interface.
- * Wrapper for third-party drawing API, such as vulkan, opengl etc.
+ * Wrapper for third-party drawing API, such as Vulkan, OpenGL etc.
  */
 class RenderDevice {
 public:
+    typedef ObjectID ID;
 
     virtual ~RenderDevice() = default;
 
+    virtual ID createVertexBuffer(uint32 size, void* data) = 0;
+    virtual ID createIndexBuffer(uint32 size, void* data) = 0;
+
+
+
     /** @return Readable hardware API name */
-    virtual const std::string& getDeviceName() const { };
+    virtual const std::string& getDeviceName() const;
     /** @return Video card vendor name */
-    virtual const std::string& getVendor() const { };
+    virtual const std::string& getVendor() const;
 
 };
 
