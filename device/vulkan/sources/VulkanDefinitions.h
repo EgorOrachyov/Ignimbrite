8c111986b71edf2e14d0598f8caf5c21642f5488
egororachyov <egororachyov5@gmail.com>
2019-11-03 12:29:44 +0300

    Refactor cmake struct for third-party libs

---
+++ device/vulkan/sources/VulkanDefinitions.h
@@ -0,0 +1,17 @@
+//
+// Created by Egor Orachyov on 2019-11-03.
+//
+
+#ifndef RENDERINGLIBRARY_VULKANDEFINITIONS_H
+#define RENDERINGLIBRARY_VULKANDEFINITIONS_H
+
+#include <vulkan/vulkan.h>
+
+class VulkanDefinitions {
+public:
+
+    static
+
+};
+
+#endif //RENDERINGLIBRARY_VULKANDEFINITIONS_H
--- device/vulkan/sources/VulkanRenderDevice.cpp
+++ device/vulkan/sources/VulkanRenderDevice.cpp
@@ -2,4 +2,4 @@
 // Created by Egor Orachyov on 2019-11-02.
 //
 
-#include "vulkan/include/VulkanRenderDevice.h"
+#include "include/VulkanRenderDevice.h"
