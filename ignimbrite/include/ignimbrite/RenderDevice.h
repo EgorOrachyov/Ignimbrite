97ad3fccf4f46960600477edc40d782c7ecec6f1
egororachyov <egororachyov5@gmail.com>
2019-12-23 15:28:51 +0300

    Indentation fixes

--- ignimbrite/include/ignimbrite/RenderDevice.h
+++ ignimbrite/include/ignimbrite/RenderDevice.h
@@ -1,6 +1,6 @@
 /**********************************************************************************/
 /* This file is part of Ignimbrite project                                        */
-/* https://github.com/EgorOrachyov/Ignimbrite                                        */
+/* https://github.com/EgorOrachyov/Ignimbrite                                     */
 /**********************************************************************************/
 /* Licensed under MIT License                                                     */
 /* Copyright (c) 2019 - 2020 Egor Orachyov, Sultim Tsyrendashiev                  */
